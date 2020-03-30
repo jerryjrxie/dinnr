557607af2cb8dbaf398314cafb560b594e2daf07
Jerry Xie <jerryjrxie@gmail.com>
2020-03-30 00:33:18 -0400

    add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Tinder for dinnr')
+    .setDescription('dinner backend API description')
+    .setVersion('0.1')
+    .addTag('dinnr')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
